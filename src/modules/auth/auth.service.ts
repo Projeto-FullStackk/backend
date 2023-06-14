556e30e27b883a01cd4e31c2d0dcb9fe853f0297
joaobuga35 <joaobuga35@gmail.com>
2023-06-14 12:56:13 -0300

    feat: add auth route

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,4 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { UserService } from '../user/user.service';
+import { compare } from 'bcryptjs';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private usersService: UserService, private jwtService: JwtService) {}
+
+  validateUser(email: string, password: string) {
+    const user = this.usersService.findByEmail(email);
+    if (user) {
+      const matchPassword = compare(password, user.password);
+      if (matchPassword) {
+        return { email: user.email };
+      }
+      return null;
+    }
+
+    async login(email: string){
+        const user = await this.usersService.findByEmail(email)
+
+        return {
+            token: this.jwtService.sign({ email }, { subject: user.id }),
+        };
+    }
+  }
+}
