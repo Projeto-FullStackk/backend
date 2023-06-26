5ebb9590b3c561e844749538c1f79daa808978b1
kelly galliani <kelly_92b@hotmail.com>
2023-06-26 12:55:23 -0300

    feat: table, dto and entity comments created

---
+++ src/modules/ads/dto/create-comment.dto.ts
@@ -0,0 +1,15 @@
+import { IsString, IsNotEmpty } from 'class-validator';
+
+export class CreateCommentDto {
+  @IsString()
+  @IsNotEmpty()
+  comment: string;
+
+  @IsString()
+  @IsNotEmpty()
+  userId: string;
+
+  @IsString()
+  @IsNotEmpty()
+  adsId: string;
+}
